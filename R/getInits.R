20649a1ca70fd6eba17cda8d90e76020846fac9f
nathdep <98131306+nathdep@users.noreply.github.com>
2024-08-23 13:26:36 -0500

    Create getInits.R

---
+++ R/getInits.R
@@ -0,0 +1,9 @@
+getInits <- function(stansum){
+  vars <- stansum$variable
+  dropind <- sub("\\[.*\\]", stansum)
+  inits <- list()
+  for(i in 1:length(dropind)){
+    inits[[dropind[i]]] <- c(inits[[dropind[i]]], stansum$variable)
+  }
+  return(inits)
+}
