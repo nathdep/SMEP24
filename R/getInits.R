c42007c0ca44f9b4e9a3426b54162892b1761d4e
nathdep <98131306+nathdep@users.noreply.github.com>
2024-08-23 14:22:55 -0500

    Update getInits.R

--- R/getInits.R
+++ R/getInits.R
@@ -3,7 +3,7 @@ getInits <- function(stansum){
   dropind <- sub("\\[.*\\]", stansum)
   inits <- list()
   for(i in 1:length(dropind)){
-    inits[[dropind[i]]] <- c(inits[[dropind[i]]], stansum$variable)
+    inits[[dropind[i]]] <- c(inits[[dropind[i]]], stansum$mean)
   }
   return(inits)
 }
