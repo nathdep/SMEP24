b99b391eeb48370e2e0be1d5d86b9b0325496958
nathdep <98131306+nathdep@users.noreply.github.com>
2024-09-03 15:44:19 -0500

    Update getStdSumScore.R

--- R/getStdSumScore.R
+++ R/getStdSumScore.R
@@ -1,14 +1,5 @@
-getStdSumScore <- function(resps){
-  if(length(dim(resps)) == 2){
-    res <- (rowSums(resps) - mean(rowSums(resps)))/sd(rowSums(resps))
-    return(res)
-  }
-  if(length(dim(resps)) > 2){
-    res <- array(data=NA, dim=c(length(dim(resps)), nrow(resps[,,1])))
-    for(dim in 1:length(dim(resps))){
-      current <- resps[,,1]
-      res[dim,] <- (rowSums(current) - mean(rowSums(current)))/sd(rowSums(current))
-    }
-    return(res)
-  }
+getStdSumScore <- function(Y){
+  num <- rowSums(resps, na.rm=TRUE) - mean(rowSums(resps, na.rm=TRUE))
+  denom <- sd(rowSums(resps, na.rm=TRUE))
+  return(num/denom)
 }
