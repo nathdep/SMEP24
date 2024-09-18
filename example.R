b83f76622e387c1d03c6541df1ce78fec9f83662
nathdep <98131306+nathdep@users.noreply.github.com>
2024-09-18 13:18:34 -0500

    change

--- example.R
+++ example.R
@@ -90,9 +90,9 @@ modsum_save_lambda <- modsum_save_lambda[,c(1, ncol(modsum_save_lambda), 2:(ncol
 modsum_save_tau <- modsum_save_tau[,c(1, ncol(modsum_save_tau), 2:(ncol(modsum_save_tau)-1))]
 modsum_save_theta <- modsum_save_theta[,c(1, ncol(modsum_save_theta), 2:(ncol(modsum_save_theta)-1))]
 
-write.csv(modsum_save_lambda, paste0(findings, "Reduc_Modsum_lambda_", seed, "_", model, "_", empiricalMethod, "_", startingMethod, ".csv"))
-write.csv(modsum_save_tau, paste0(findings, "Reduc_Modsum_tau_", seed, "_", model, "_", empiricalMethod, "_", startingMethod, ".csv"))
-write.csv(modsum_save_theta, paste0(findings, "Reduc_Modsum_theta_", seed, "_", model, "_", empiricalMethod, "_", startingMethod, ".csv"))
+write.csv(modsum_save_lambda, paste0(findings, "Reduc_Modsum_lambda_", model, "_", empiricalMethod, "_", startingMethod,"_", args[1], "_", args[2], ".csv"))
+write.csv(modsum_save_tau, paste0(findings, "Reduc_Modsum_tau_", model, "_", empiricalMethod, "_", startingMethod,"_", args[1], "_", args[2], ".csv"))
+write.csv(modsum_save_theta, paste0(findings, "Reduc_Modsum_theta_", model, "_", empiricalMethod, "_", startingMethod,"_", args[1], "_", args[2], ".csv"))
 
 write.csv(modsum_full, paste0(findings, "Full_Modsum_", seed, "_", model, "_", empiricalMethod, "_", startingMethod, ".csv"))
 
@@ -104,7 +104,7 @@ if(nBadRhats != 0 && !interactive()){
   write.csv(badRhatModsum, paste0(findings, "BadRhat_Modsum_", seed, "_", model, "_", empiricalMethod, "_", startingMethod, ".csv")) # write non-convergent parameter posterior descriptives to .csv file
 
   sink(paste0(findings, "Names_BadRhat_", model, "_", empiricalMethod, "_", startingMethod, ".csv"), append=TRUE) # begin appending <model>_<method>_badCount.csv file
-  cat(paste0(nBadRhats,",", model, ",", empiricalMethod, ",", startingMethod, "\n")) # write result
+  cat(paste0(nBadRhats,",", model, ",", empiricalMethod, ",", startingMethod, ",", seed, "\n")) # write result
   sink() # close connection
 
 }
