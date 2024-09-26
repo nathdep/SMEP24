c1ca7e55fc2f2d08bede440628d281cfb21c660a
nathdep <98131306+nathdep@users.noreply.github.com>
2024-09-26 16:05:23 -0500

    change

---
+++ postproc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#$ -e /dev/null
+#$ -o /dev/null
+#$ -pe smp 4
+#$ -q CASMA
+#$ -N POSTPROC
+
+ERR="/Users/depy/SMEP24/e/POSTPROC_STDERR_${JOB_ID}_${SGE_TASK_ID}.txt"
+OUT="/Users/depy/SMEP24/o/POSTPROC_STDOUT_${JOB_ID}_${SGE_TASK_ID}.txt"
+
+exec 1> $OUT
+exec 2> $ERR
+
+cd /Users/depy/SMEP24
+
+apptainer run --bind /Users/depy/SMEP24:/root /Users/depy/containers/R.sif Rscript --vanilla postproc.R $JOB_ID $SGE_TASK_ID
