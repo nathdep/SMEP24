5405b9375747929f65ecb15c65c9558984953f6b
nathdep <98131306+nathdep@users.noreply.github.com>
2024-09-28 14:19:43 -0500

    Update postproc.R

--- postproc.R
+++ postproc.R
@@ -1,5 +1,7 @@
 library(SMEP24)
 
+CONTROL=FALSE
+
 files <- list.files(path="/root/Findings/", pattern=".csv$")
 csvFiles <- lapply(files, function(x)fread(file=paste0("/root/Findings/", x), data.table=FALSE))
 
@@ -46,4 +48,43 @@ for(i in 1:length(splitLists)){
   saveRDS(splitLists[i], file=paste0(dfNames[i], ".RDS"))
 }
 
+models <- c("twopl", "bifactor")
+examineeSizes <- c(500, 2000)
+paramNames <- c("theta", "lambda", "tau")
+
+if(CONTROL){
+  control_starting_methods=c("ALLPOS", "CONTROL")
+  control_empirical_methods="NA"
+  combo_matrix <- expand.grid(model=models, empiricalMethod=control_empirical_methods, startingMethod=control_starting_methods, sampleSize=examineeSizes)
+}
+
+if(!CONTROL){
+  tested_empirical_methods <-  c("empiricalPos", "empiricalAlpha")
+  tested_starting_methods <- c("advi", "allRand", "StdSumScore")
+  combo_matrix <- expand.grid(model=models, empiricalMethod=tested_empirical_methods, startingMethod=tested_starting_methods, sampleSize=examineeSizes)
+}
+
+selRow <-as.vector(as.matrix(methodSelect(base10=args[2], methodsMatrix=combo_matrix)))
+
+filteredInds <- which(sapply(Reduc_Modsum$Infos, function(x) all(selRow %in% x)))
+filteredModsumsFull <- lapply(Reduc_Modsum$Modsums[filteredInds], function(x)x$df)
+
+paramInds <- vector(length=length(paramNames), mode="list")
+names(paramInds) <- paramNames
+
+filteredModsums <- vector(length=length(paramNames), mode="list")
+names(filteredModsums) <- paramNames
+
+for(j in 1:length(paramNames)){
+  for(i in 1:length(filteredInds)){
+    if(Reduc_Modsum$Modsums[[filteredInds[i]]]$reduc == paramNames[j]){
+      paramInds[[paramNames[j]]] <- c(paramInds[[paramNames[j]]], i)
+    }
+  }
+}
+
+for(j in 1:length(paramNames)){
+  filteredModsums[[paramNames[j]]] <- filteredModsumsFull[paramInds[[paramNames[j]]]]
+  write.csv(bind_rows(filteredModsums[[paramNames[j]]])[,2:ncol(bind_rows(filteredModsums[[paramNames[j]]]))], paste0(paste0(selRow, collapse="_"), "_",paramNames[j],".csv"), row.names=FALSE)
+}
 
