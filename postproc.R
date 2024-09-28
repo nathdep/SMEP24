d583086718535e77cf0f9d029cfa79b6851e842d
nathdep <98131306+nathdep@users.noreply.github.com>
2024-09-27 21:34:47 -0500

    Update postproc.R

--- postproc.R
+++ postproc.R
@@ -7,16 +7,18 @@ f <- gsub(pattern=".*__(.*)__.*", replacement="\\1", x=files)
 
 gatheredInfo <- lapply(strsplit(x=f, split="_"), function(x)sub(pattern=".csv",replacement="",x=x))
 prefix <- lapply(files, function(x)sub(pattern="(.*?)__.*", replacement="\\1",x=x))
+suffix <- lapply(files, function(x)sub(pattern=".*__(.*?).csv$", replacement="\\1", x=x))
 
 catList <- vector(length=length(f), mode="list")
 
 for(i in 1:length(f)){
   catList[[i]]$Info <- as.data.frame(rbind(c(prefix[[i]], gatheredInfo[[i]])))
-  if(!any(c("lambda", "tau", "theta") %in% gatheredInfo[[i]][length(gatheredInfo[[i]])])){
+  if(!any(c("lambda", "tau", "theta") %in% suffix[[i]])){
     colnames(catList[[i]]$Info) <- c("type","model", "empiricalMethod", "startingMethod", "sampleSize", "seed", "taskNo")
   }
-  if(any(c("lambda", "tau", "theta") %in% gatheredInfo[[i]][length(gatheredInfo[[i]])])){
-    colnames(catList[[i]]$Info) <-  c("type","model", "empiricalMethod", "startingMethod", "sampleSize", "seed", "taskNo", gatheredInfo[[i]][length(gatheredInfo[[i]])])
+  if(any(c("lambda", "tau", "theta") %in% suffix[[i]])){
+    catList[[i]]$Info <- cbind(catList[[i]]$Info, suffix[[i]])
+    colnames(catList[[i]]$Info) <-  c("type","model", "empiricalMethod", "startingMethod", "sampleSize", "seed", "taskNo", "reduc")
   }
   current_csv <- read_csv(paste0("/root/Findings/", files[i]))
   catList[[i]]$Modsum <- current_csv
@@ -34,8 +36,8 @@ typeInds <- sapply(dfNames, function(x)which(namesLong == x))
 for(i in 1:length(dfNames)){
   selCatList <- catList[typeInds[[dfNames[i]]]]
   selInfos <- lapply(selCatList, function(x)x$Info)
-  assign("Infos", selInfos, envir=get(dfNames[i], envir=.GlobalEnv))
   selModsums <- lapply(selCatList, function(x)x$Modsum)
+  assign("Infos", selInfos, envir=get(dfNames[i], envir=.GlobalEnv))
   assign("Modsums", selModsums, envir=get(dfNames[i], envir=.GlobalEnv))
 }
 
