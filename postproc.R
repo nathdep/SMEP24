84db868bd0c119dcdf34ff5f32b6194e182177dc
nathdep <98131306+nathdep@users.noreply.github.com>
2024-09-29 15:23:58 -0500

    Update postproc.R

--- postproc.R
+++ postproc.R
@@ -10,12 +10,14 @@ examineeSizes <- c(500, 2000)
 paramNames <- c("theta", "lambda", "tau")
 
 if(CONTROL){
+  whichDir = "Reduc_Control/"
   control_starting_methods=c("ALLPOS", "CONTROL")
   control_empirical_methods="NA"
   combo_matrix <- expand.grid(model=models, empiricalMethod=control_empirical_methods, startingMethod=control_starting_methods, sampleSize=examineeSizes)
 }
 
 if(!CONTROL){
+  whichDir = "Reduc_Tested/"
   tested_empirical_methods <-  c("empiricalPos", "empiricalAlpha")
   tested_starting_methods <- c("advi", "allRand", "StdSumScore")
   combo_matrix <- expand.grid(model=models, empiricalMethod=tested_empirical_methods, startingMethod=tested_starting_methods, sampleSize=examineeSizes)
@@ -40,6 +42,9 @@ names(filteredInfos) <- paramNames
 bound <- vector(length=length(paramNames), mode="list")
 names(bound) <- paramNames
 
+numParams <- vector(length=length(paramNames), mode="list")
+names(numParams) <- paramNames
+
 for(j in 1:length(paramNames)){
   for(i in 1:length(filteredInds)){
     if(Reduc_Modsum$Modsums[[filteredInds[i]]]$reduc == paramNames[j]){
@@ -50,11 +55,12 @@ for(j in 1:length(paramNames)){
 
 for(j in 1:length(paramNames)){
   filteredModsums[[paramNames[j]]] <- filteredModsumsFull[paramInds[[paramNames[j]]]]
+  numParams[[paramNames[j]]] <- nrow(filteredModsums[[paramNames[j]]][[1]])
   filteredInfos[[paramNames[j]]] <- filteredInfosFull[paramInds[[paramNames[j]]]]
   bound[[paramNames[j]]] <- bind_rows(filteredModsums[[paramNames[j]]])
   bound[[paramNames[j]]] <- bound[[paramNames[j]]][,-which(colnames(bound[[paramNames[j]]]) == "V1")]
-  bound[[paramNames[j]]]$File <- as.vector(unlist(filteredInfos[[paramNames[j]]]))
-  write.csv(bound[[paramNames[j]]], paste0(paste0(selRow, collapse="_"), "_",paramNames[j],".csv"), row.names=FALSE)
+  bound[[paramNames[j]]]$File <- rep(as.vector(unlist(filteredInfos[[paramNames[j]]])), each=numParams[[paramNames[j]]])
+  write.csv(bound[[paramNames[j]]], paste0(whichDir, paste0(selRow, collapse="_"), "_",paramNames[j],".csv"), row.names=FALSE)
 }
 
 paramLengths <- lapply(filteredModsums, length)
@@ -63,5 +69,5 @@ cat("\nPARAM LENGTHS\n")
 print(paramLengths)
 cat("\n")
 
-saveRDS(filteredModsums, file=paste0(paste0(selRow, collapse="_"), "_", "filteredModsums.RDS"))
+saveRDS(filteredModsums, file=paste0(whichDir, paste0(selRow, collapse="_"), "_", "filteredModsums.RDS"))
 
