2f2b71f0be1591cc40603a2f8cf256c468efb75b
nathdep <98131306+nathdep@users.noreply.github.com>
2024-10-03 19:57:45 -0500

    Create genTable.R

---
+++ genTable.R
@@ -0,0 +1,25 @@
+library(SMEP24)
+
+dirs <- as.list(paste0("D:\\SMEP_0\\", c("Reduc_Control", "Reduc_Tested"), "\\"))
+
+files <- lapply(dirs, function(x) {
+  setwd(x)
+  list.files(pattern="\\.csv")
+})
+
+dfs <- vector(mode="list", length=2)
+
+for(i in 1:2){
+  setwd(dirs[[i]])
+  dfs[[i]] <- bind_rows(lapply(files[[i]], function(x) fread(x, data.table=FALSE)))
+}
+
+dfFile <- lapply(dfs, function(x) strsplit(x$File, split="_"))
+extraCols <- as.data.frame(t(do.call(cbind, dfFile[[1]])))
+extraCols <- extraCols[,3:ncol(extraCols)]
+colnames(extraCols) <- c("model","empir", "start", "sample", "seed", "taskNo")
+
+compiled <- as.data.frame(cbind(bind_rows(dfs), bind_rows(extraCols)))
+
+compiled[which(compiled$empir == "NA"),14] <- NA
+
